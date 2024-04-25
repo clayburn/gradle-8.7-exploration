83275e847cb653c2e04c6fe32fffd1c706597470
Clay Johnson <cjohnson@gradle.com>
2024-04-25 14:04:32 -0500

    Update versions without dependencies sample

--- 04-error-and-warning-improvements/03-suppressable-dependencies-without-version-error/build.gradle.kts
+++ 04-error-and-warning-improvements/03-suppressable-dependencies-without-version-error/build.gradle.kts
@@ -1,27 +1,51 @@
 plugins {
     java
     `maven-publish`
-    id("java-gradle-plugin")
-    id("com.gradle.plugin-publish") version "1.2.1"
 }
 
 group = "com.example"
 version = "0.1.0"
 
-repositories {
-    mavenCentral()
+publishing {
+    publications.create<MavenPublication>("library").from(components["java"])
+    repositories.maven("repo")
+}
+
+val internal = configurations.dependencyScope("internal").get()
+sourceSets.all {
+    configurations[compileClasspathConfigurationName].extendsFrom(internal)
+    configurations[runtimeClasspathConfigurationName].extendsFrom(internal)
+    configurations[annotationProcessorConfigurationName].extendsFrom(internal)
 }
 
 dependencies {
-    implementation("org.test:test")
-}
+    // Use Spring Boot Platform, but imagine a company internal platform
+    // Internal users are assumed to always select a platform version
 
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
+    if (hasProperty("externalizePlatformDependency")) {
+        implementation(platform("org.springframework.boot:spring-boot-dependencies:3.2.4"))
+    } else {
+        internal(platform("org.springframework.boot:spring-boot-dependencies:3.2.4"))
     }
+
+    implementation("org.apache.commons:commons-lang3")
+    implementation("org.apache.commons:commons-pool2")
 }
 
+//repositories {
+//    mavenCentral()
+//}
+//
+//dependencies {
+//    implementation("org.test:test")
+//}
+//
+//java {
+//    toolchain {
+//        languageVersion = JavaLanguageVersion.of(21)
+//    }
+//}
+//
 if (hasProperty("suppressMissingDependencyVersions")) {
     tasks.withType<(GenerateModuleMetadata)>().configureEach {
         suppressedValidationErrors.add("dependencies-without-versions")
--- 04-error-and-warning-improvements/03-suppressable-dependencies-without-version-error/run.sh
+++ 04-error-and-warning-improvements/03-suppressable-dependencies-without-version-error/run.sh
@@ -4,13 +4,25 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 pushd $script_dir
 
 echo "------------------------------------------------------------"
+echo "Publish with platform metatdata included"
+echo "------------------------------------------------------------"
+./gradlew publish -PexternalizePlatformDependency
+QUERY='.variants[] | select(.name == "runtimeElements")'
+MODULE_DESCRIPTOR=repo/com/example/suppressable-dependencies-without-version-error/0.1.0/suppressable-dependencies-without-version-error-0.1.0.module
+jq "$QUERY" < $MODULE_DESCRIPTOR
+
+echo "------------------------------------------------------------"
 echo "Don't suppress missing dependency versions when generating metadata file for plugin maven publication"
 echo "------------------------------------------------------------"
-./gradlew generateMetadataFileForPluginMavenPublication
+
+./gradlew publish
 
 echo "------------------------------------------------------------"
 echo "Suppress missing dependency versions when generating metadata file for plugin maven publication"
 echo "------------------------------------------------------------"
-./gradlew generateMetadataFileForPluginMavenPublication -PsuppressMissingDependencyVersions
+
+./gradlew publish -PsuppressMissingDependencyVersions
+
+jq "$QUERY" < $MODULE_DESCRIPTOR
 
 popd
